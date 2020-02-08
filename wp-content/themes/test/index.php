79acb00577c5551871924cc9432fc342a1d54540
DmitriyTishchenko <kt041084tdn@gmail.com>
2020-02-09 01:25:02 +0200

    Lesson 9. Post Tags

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -1,15 +1,28 @@
 <?php get_header();?>
 
-<h1>Hello, world!</h1>
+<div class="container">
+    <div class="row">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <!-- Post -->
+            <div class="col-md-12">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="Card image cap">
+                    <div class="card-body">
+                        <a href="<?php the_permalink();?>"><h5 class="card-title"><?php the_title(); ?></h5></a>
+                        <p class="card-text"><?php the_content(""); ?></p>
+                        <a href="<?php the_permalink();?>" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        <?php endwhile; ?>
+            <!-- Post navigation -->
+        <?php else: ?>
+            <!-- No post found -->
+            <p>Записей нет.</p>
+        <?php endif; ?>
+    </div>
+</div>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-	<!-- Post -->
-	<h4><?php the_title() ?></h4>
-<?php endwhile; ?>
-    <!-- Post navigation -->
-<?php else: ?>
-	<!-- No post found -->
-	<p>Записей нет.</p>
-<?php endif; ?>
+
 
 <?php get_footer();?>
