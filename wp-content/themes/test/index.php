6cc5951f712f7acff0ce765e33c2700ae2b13532
DmitriyTishchenko <kt041084tdn@gmail.com>
2020-02-09 00:33:08 +0200

    The Loop Theme in WordPress

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -2,6 +2,14 @@
 
 <h1>Hello, world!</h1>
 
-
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<!-- Post -->
+	<h4><?php the_title() ?></h4>
+<?php endwhile; ?>
+    <!-- Post navigation -->
+<?php else: ?>
+	<!-- No post found -->
+	<p>Записей нет.</p>
+<?php endif; ?>
 
 <?php get_footer();?>
