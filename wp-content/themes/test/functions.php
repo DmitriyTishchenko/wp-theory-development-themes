d2dba83d97d3aa07a9e37439bc19803281f89124
DmitriyTishchenko <kt041084tdn@gmail.com>
2020-02-09 23:56:43 +0200

    Lesson 12. Working with media files in WorldPress. Part1

--- wp-content/themes/test/functions.php
+++ wp-content/themes/test/functions.php
@@ -16,3 +16,12 @@ function test_scripts() {
     wp_enqueue_script( 'test-popper', "//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array('jquery'), null, true);
     wp_enqueue_script( 'test-bootstrapjs', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', array('jquery'), null, true);
 }
+
+/*## отключаем создание миниатюр файлов для указанных размеров
+add_filter( 'intermediate_image_sizes', 'delete_intermediate_image_sizes' );
+function delete_intermediate_image_sizes( $sizes ){
+    // размеры которые нужно удалить
+    return array_diff( $sizes, array(
+        'medium_large',
+    ) );
+}*/
